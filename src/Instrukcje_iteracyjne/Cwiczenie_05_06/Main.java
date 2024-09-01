2ff470e2b1c4020390b390820b111bdd90fb20fc
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 14:17:30 +0200

    Ciwczenie 05_06

---
+++ src/Instrukcje_iteracyjne/Cwiczenie_05_06/Main.java
@@ -0,0 +1,11 @@
+package Instrukcje_iteracyjne.Cwiczenie_05_06;
+
+public class Main {
+    public static void main(String[] args){
+        for(int i=-1500;i<=1500;i++){
+            if(i%3==0 && i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
