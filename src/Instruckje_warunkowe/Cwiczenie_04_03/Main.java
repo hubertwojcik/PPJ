7833c94e5fb56fa41c6270a2ce8ebfa2114caf6c
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 11:32:08 +0200

    Cwiczenie 04_03

--- src/Instruckje_warunkowe/Cwiczenie_04_03/Main.java
+++ src/Instruckje_warunkowe/Cwiczenie_04_03/Main.java
@@ -1,2 +1,14 @@
-package Instruckje_warunkowe.Cwiczenie_04_03;public class Main {
+package Instruckje_warunkowe.Cwiczenie_04_03;
+
+public class Main {
+    public static void main(String[] args){
+        int zmInt=4;
+        double zmDouble=-1.0;
+        if(zmInt>0)
+            if(zmDouble>0)
+                System.out.println("Here I am");
+        else
+            System.out.println("No, I am here");
+        System.out.println("No, acutally, I am here");
+    }
 }
