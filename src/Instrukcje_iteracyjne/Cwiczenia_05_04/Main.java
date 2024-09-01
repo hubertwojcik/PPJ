1739dc28329bdcd703d8fe386f674b9915febc72
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 13:41:07 +0200

    Cwiczenie 05_04

---
+++ src/Instrukcje_iteracyjne/Cwiczenia_05_04/Main.java
@@ -0,0 +1,11 @@
+package Instrukcje_iteracyjne.Cwiczenia_05_04;
+
+public class Main {
+    public static void main(String[] args){
+        double item=1;
+        for(int i=1;i<=10;i++){
+            item=item*0.5;
+            System.out.print(item+" ");
+        }
+    }
+}
