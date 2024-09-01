5801923eeaa34951147f111fde5513bbfce79c1e
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 10:20:41 +0200

    Cwiczenie_02_01

--- src/Litraly_i_operacje_bitowe/Cwiczenie_02_01/Main.java
+++ src/Litraly_i_operacje_bitowe/Cwiczenie_02_01/Main.java
@@ -3,5 +3,15 @@ package Litraly_i_operacje_bitowe.Cwiczenie_02_01;
 //binarnego (system dwójkowy),
 //oktalnego (system ósemkowy),
 //heksadecymalnego (system szesnastkowy).
+
 public class Main {
+    public static void main (String[] args){
+        int binary=0b10100101;
+        int oct=0245;
+        int hex=0xA5;
+
+        System.out.println("Binary: " + binary);
+        System.out.println("Octal: " + oct);
+        System.out.println("Hexadecimal: " + hex);
+    }
 }
