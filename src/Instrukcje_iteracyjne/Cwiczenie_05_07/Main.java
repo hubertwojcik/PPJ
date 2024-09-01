b33139f05e965275a7d7fe29b5876ea911ff0bd5
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 14:21:11 +0200

    Cwiczenia 05_07

---
+++ src/Instrukcje_iteracyjne/Cwiczenie_05_07/Main.java
@@ -0,0 +1,12 @@
+package Instrukcje_iteracyjne.Cwiczenie_05_07;
+
+public class Main {
+    public static void main(String[] args){
+        for (int i=1;i<=5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
