db62646de1b24d064cac5f2c85eaf0c504ac5595
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 10:09:43 +0200

    first task

---
+++ src/Litraly_i_operacje_bitowe/Cwiczenie_02_01/Main.java
@@ -0,0 +1,7 @@
+package Litraly_i_operacje_bitowe.Cwiczenie_02_01;
+//Dana jest wartość 165. Utwórz program wyświetlający na ekranie tę wartość z wykorzystaniem literału:
+//binarnego (system dwójkowy),
+//oktalnego (system ósemkowy),
+//heksadecymalnego (system szesnastkowy).
+public class Main {
+}
