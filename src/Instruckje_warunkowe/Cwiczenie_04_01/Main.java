d6cddcb5341cbdf8ea695d7349bbdccecd799a73
Hubert Wójcik <hwojcikk@gmail.com>
2024-09-01 11:31:53 +0200

    Cwiczenie 04_01

---
+++ src/Instruckje_warunkowe/Cwiczenie_04_01/Main.java
@@ -0,0 +1,11 @@
+package Instruckje_warunkowe.Cwiczenie_04_01;
+
+public class Main {
+    public static void main(String[] args){
+        int i=10; //10
+        i=i++; //10
+        i=++i; //12
+        i=i++ + ++i; //24
+
+    }
+}
---
+++ src/Instruckje_warunkowe/Cwiczenie_04_03/Main.java
@@ -0,0 +1,2 @@
+package Instruckje_warunkowe.Cwiczenie_04_03;public class Main {
+}
